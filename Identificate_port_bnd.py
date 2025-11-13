b8a66ba54bc80033e6a374e6e8a25cfb29b9d367
Mehdi <mahdi.bashari.eng@gmail.com>
2025-11-13 09:32:42 +0330

    Identificate_port_bnd.py

--- Identificate_port_bnd.py
+++ Identificate_port_bnd.py
@@ -5,13 +5,13 @@ import re
 import unicodedata
 import string
 
-def Identicate_Port_and_bundrate(length=50):
+def Identicate_Port_and_baudrate(length=50):
     """
-    Scan available serial ports and test common bund rates.
+    Scan available serial ports and test common baud rates.
 
     This function automatically scans connected serial (COM) ports,
-    tries multiple common bund rates, and returns any ports and
-    bund rates that appear to be transmitting valid data.
+    tries multiple common baud rates, and returns any ports and
+    baud rates that appear to be transmitting valid data.
 
     Parameter
     ----------
@@ -21,17 +21,17 @@ def Identicate_Port_and_bundrate(length=50):
     Returns
     -------
     tuple
-        (ports_found, bund_rates_found, data)
+        (ports_found, baud_rates_found, data)
         - ports_found: list of detected port names (e.g. ['COM3'])
-        - bund_rates_found: list of working bund rates (e.g. [9600])
+        - baud_rates_found: list of working baud rates (e.g. [9600])
         - data: list of received data
 
     Example
     -------
     Python usage:
 
-    >>> from identify_serial import Identicate_Port_and_bundrate
-    >>> ports, baud_rates, data = Identicate_Port_and_bundrate()
+    >>> from identify_serial import Identicate_Port_and_baudrate
+    >>> ports, baud_rates, data = Identicate_Port_and_baudrate()
     >>> print(ports, baud_rates, data)
     ['COM3'] [9600] ['F6 8E 87 32 FA 26 8E BE 208']
         Arduino test code:
@@ -39,7 +39,7 @@ def Identicate_Port_and_bundrate(length=50):
     Arduino Test:
     ```cpp
     void setup() {
-      Serial.begin(9600); // Change this to test other bund rates
+      Serial.begin(9600); // Change this to test other baud rates
     }
 
     void loop() {
@@ -58,10 +58,10 @@ def Identicate_Port_and_bundrate(length=50):
 
     Upload this code to your Arduino board, then run the Python script.
     The function will automatically detect both the correct port
-    and the bund rate.
+    and the baud rate.
     """
     ports_final=[]
-    bund_rates_final=[]
+    baud_rates_final=[]
     data_final=[]
     ports = serial.tools.list_ports.comports()
     for port in ports:
@@ -69,8 +69,8 @@ def Identicate_Port_and_bundrate(length=50):
             port_f = port.name
     ports_final.append(port_f)
 
-    bund_rates = [9600, 19200, 38400, 57600, 115200, 230400]
-    for rate in bund_rates:
+    baud_rates = [9600, 19200, 38400, 57600, 115200, 230400]
+    for rate in baud_rates:
         try:
             ser = serial.Serial(port_f, rate, timeout=1)
             time.sleep(1)
@@ -78,14 +78,12 @@ def Identicate_Port_and_bundrate(length=50):
             ser.close()
             t1= data.split('\r\n')[0]
             if len(t1)<length:
-                bund_rates_final.append(rate)  
+                baud_rates_final.append(rate)  
                 data_final.append(t1)   
         except Exception as e:
             print(f"{rate}: {e}")
-    return ports_final,bund_rates_final,data_final
+    return ports_final,baud_rates_final,data_final
 
-a= Identicate_Port_and_bundrate()
-print(a)
 #--------------------------------This part is intended to detect invalid or garbage data------------------------------------
 # premis = has_unprintable_chars(data)
 # print(premis)
